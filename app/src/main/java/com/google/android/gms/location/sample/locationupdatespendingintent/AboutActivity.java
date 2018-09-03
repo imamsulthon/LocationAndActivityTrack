dc74a72f60151df7767fac43308e65beda8af5fc
Imam Sulthon <imamsulthon@yahoo.com>
2018-09-03 17:20:20 +0700

    adding about and change interval time of location updates

---
+++ app/src/main/java/com/google/android/gms/location/sample/locationupdatespendingintent/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.google.android.gms.location.sample.locationupdatespendingintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/google/android/gms/location/sample/locationupdatespendingintent/MainActivity.java
+++ app/src/main/java/com/google/android/gms/location/sample/locationupdatespendingintent/MainActivity.java
@@ -37,6 +37,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -72,19 +73,19 @@ public class MainActivity extends FragmentActivity implements
     /**
      * The desired interval for location updates. Inexact. Updates may be more or less frequent.
      */
-    private static final long UPDATE_INTERVAL = 60000; // Every 60 seconds.
+    private static final long UPDATE_INTERVAL = 30000; // Every 60 seconds.
 
     /**
      * The fastest rate for active location updates. Updates will never be more frequent
      * than this value, but they may be less frequent.
      */
-    private static final long FASTEST_UPDATE_INTERVAL = 30000; // Every 30 seconds
+    private static final long FASTEST_UPDATE_INTERVAL = 20000; // Every 30 seconds
 
     /**
      * The max time before batched results are delivered by location services. Results may be
      * delivered sooner than this interval.
      */
-    private static final long MAX_WAIT_TIME = UPDATE_INTERVAL * 5; // Every 5 minutes.
+    private static final long MAX_WAIT_TIME = UPDATE_INTERVAL * 3; // Every 5 minutes.
 
     /**
      * Stores parameters for requests to the FusedLocationProviderApi.
@@ -102,6 +103,7 @@ public class MainActivity extends FragmentActivity implements
     private Button mLogoutButton;
     private TextView mLocationUpdatesResultView;
     private ListView mdetectedActivityListView;
+    private ImageView iconImage;
 
     boolean firstOpen = true;
     // Activity Recognition Api
@@ -550,4 +552,10 @@ public class MainActivity extends FragmentActivity implements
         alertDialog.show();
 
     }
+
+    public void aboutPage(View view) {
+        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(intent);
+    }
+
 }
